8ba7289372f5cf0442402c08bbce8c69e22a674b
AForkintheCode <96763461+AForkintheCode@users.noreply.github.com>
2023-03-31 02:56:07 -0700

    blah

--- public/js/signup.js
+++ public/js/signup.js
@@ -24,22 +24,4 @@ const signupFormHandler = async (event) => {
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
 
-  const login = async (event) => {
-    event.preventDefault();
   
-    const response = await fetch('/api/users/login', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  };
-
-  document
-  .querySelector('.link')
-  .addEventListener('click', login);
